dc2832ac59a396616805e11057778693a0db7bd7
Dmitriy Zheleznyakov <dmitriy.zheleznyakov@Dmitriys-MBP.broadband>
2020-06-07 15:26:59 +0100

    Spring5 Pet Clinic: initialise

---
+++ PetClinic/src/main/java/zh/learn/spring5/petclinit/PetClinicApplication.java
@@ -0,0 +1,13 @@
+package zh.learn.spring5.petclinit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetClinicApplication.class, args);
+	}
+
+}
---
+++ PetClinic/src/test/java/zh/learn/spring5/petclinit/PetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package zh.learn.spring5.petclinit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
