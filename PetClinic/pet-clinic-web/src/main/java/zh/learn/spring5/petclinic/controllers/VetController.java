017d309c9c78fcb298949f5e9be1bcfb919f7013
dzheleznyakov <zheleznyakov.dmitry@gmail.com>
2020-06-14 09:53:00 +0100

    Spring5 PetClinic: Display List of Vets on the Vets Page. Closes #15.

--- PetClinic/pet-clinic-web/src/main/java/zh/learn/spring5/petclinic/controllers/VetController.java
+++ PetClinic/pet-clinic-web/src/main/java/zh/learn/spring5/petclinic/controllers/VetController.java
@@ -1,13 +1,22 @@
 package zh.learn.spring5.petclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import zh.learn.spring5.petclinic.services.VetService;
 
 @Controller
 @RequestMapping("/vets")
 public class VetController {
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"", "/", "/index", "/index.html"})
-    public String listVets() {
+    public String listVets(Model model) {
+        model.addAttribute("vets", vetService.findAll());
         return "vets/index";
     }
 }
