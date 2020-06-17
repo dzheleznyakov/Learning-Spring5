1cf9e1f1497327541b0b23cb3831a6ae43f7d34c
dzheleznyakov <zheleznyakov.dmitry@gmail.com>
2020-06-17 06:34:53 +0100

    Spring5 PetClinic: Create Vet Speciality Entity, Associate to Vet. Closes #27.

--- PetClinic/pet-clinic-data/src/main/java/zh/learn/spring5/petclinic/model/Vet.java
+++ PetClinic/pet-clinic-data/src/main/java/zh/learn/spring5/petclinic/model/Vet.java
@@ -1,6 +1,18 @@
 package zh.learn.spring5.petclinic.model;
 
+import java.util.Set;
+
 public class Vet extends Person {
+    private Set<Specialty> specialties;
+
     public Vet() {
+    }
+
+    public Set<Specialty> getSpecialties() {
+        return specialties;
+    }
+
+    public void setSpecialties(Set<Specialty> specialties) {
+        this.specialties = specialties;
     }
 }
