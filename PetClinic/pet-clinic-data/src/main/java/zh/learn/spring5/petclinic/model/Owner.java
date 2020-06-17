479dce5e8d87e7a64ba523fdbe49da16631710d5
dzheleznyakov <zheleznyakov.dmitry@gmail.com>
2020-06-17 06:31:26 +0100

    Spring5 PetClinic: Create Visit Entity. Closes #26.

--- PetClinic/pet-clinic-data/src/main/java/zh/learn/spring5/petclinic/model/Owner.java
+++ PetClinic/pet-clinic-data/src/main/java/zh/learn/spring5/petclinic/model/Owner.java
@@ -1,6 +1,18 @@
 package zh.learn.spring5.petclinic.model;
 
+import java.util.Set;
+
 public class Owner extends Person {
+    private Set<Pet> pets;
+
     public Owner() {
+    }
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
     }
 }
