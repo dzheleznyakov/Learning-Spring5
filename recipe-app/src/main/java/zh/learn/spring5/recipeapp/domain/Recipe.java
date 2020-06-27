976097365440cd16495326832a8188c025bbb3e9
dzheleznyakov <zheleznyakov.dmitry@gmail.com>
2020-06-27 17:49:27 +0100

    Spring5 RecipeApp: Add Ingredient and UnitOfMeasure JPA entities.

--- recipe-app/src/main/java/zh/learn/spring5/recipeapp/domain/Recipe.java
+++ recipe-app/src/main/java/zh/learn/spring5/recipeapp/domain/Recipe.java
@@ -6,7 +6,9 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Lob;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
+import java.util.Set;
 
 @Entity
 public class Recipe {
@@ -21,6 +23,9 @@ public class Recipe {
     private String source;
     private String url;
     private String directions;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
+    private Set<Ingredient> ingredients;
 
     @Lob
     private Byte[] image;
