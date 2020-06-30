37e184d630aaa5dda03d916a3a534f85e2813d9d
dzheleznyakov <zheleznyakov.dmitry@gmail.com>
2020-06-30 11:12:50 +0100

    Spring5 PetClinic: Add Spring Data JPA service for Owners. Closes #40.

--- PetClinic/pet-clinic-data/src/main/java/zh/learn/spring5/petclinic/repositories/OwnerRepository.java
+++ PetClinic/pet-clinic-data/src/main/java/zh/learn/spring5/petclinic/repositories/OwnerRepository.java
@@ -4,4 +4,5 @@ import org.springframework.data.repository.CrudRepository;
 import zh.learn.spring5.petclinic.model.Owner;
 
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
+    Owner findByLastName(String lastName);
 }
