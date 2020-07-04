099c120e35371fa5878de26349de34d814234c2f
dzheleznyakov <zheleznyakov.dmitry@gmail.com>
2020-07-04 10:27:30 +0100

    Spring5 RecipeApp: Add IndexController test

---
+++ recipe-app/src/test/java/zh/learn/spring5/recipeapp/controllers/IndexControllerTest.java
@@ -0,0 +1,51 @@
+package zh.learn.spring5.recipeapp.controllers;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.ui.Model;
+import zh.learn.spring5.recipeapp.domain.Recipe;
+import zh.learn.spring5.recipeapp.services.RecipeService;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+public class IndexControllerTest {
+    private IndexController indexController;
+
+    @Mock
+    RecipeService recipeService;
+
+    @Mock
+    Model model;
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+
+        indexController = new IndexController(recipeService);
+    }
+
+    @Test
+    public void getIndexPage() {
+        Recipe recipe = new Recipe();
+        Set<Recipe> recipesData = new HashSet<>();
+        recipesData.add(recipe);
+
+        when(recipeService.getRecipes())
+                .thenReturn(recipesData);
+
+        String viewName = indexController.getIndexPage(model);
+
+        assertEquals("index", viewName);
+        verify(recipeService, times(1)).getRecipes();
+        verify(model, times(1)).addAttribute(eq("recipes"), eq(recipesData));
+    }
+}
