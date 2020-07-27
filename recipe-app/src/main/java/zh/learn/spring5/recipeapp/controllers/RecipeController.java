a1216e1fd50d4c9357dad4fc79688b4134d429d4
dzheleznyakov <zheleznyakov.dmitry@gmail.com>
2020-07-27 12:07:55 +0100

    Spring5 RecipeApp: New Recipe page

--- recipe-app/src/main/java/zh/learn/spring5/recipeapp/controllers/RecipeController.java
+++ recipe-app/src/main/java/zh/learn/spring5/recipeapp/controllers/RecipeController.java
@@ -2,8 +2,11 @@ package zh.learn.spring5.recipeapp.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import zh.learn.spring5.recipeapp.commands.RecipeCommand;
 import zh.learn.spring5.recipeapp.services.RecipeService;
 
 @Controller
@@ -19,5 +22,19 @@ public class RecipeController {
     public String showById(@PathVariable("id") String id, Model model) {
         model.addAttribute("recipe", recipeService.findById(Long.valueOf(id)));
         return "recipe/show";
+    }
+
+    @RequestMapping("/new")
+    public String newRecipe(Model model) {
+        model.addAttribute("recipe", new RecipeCommand());
+
+        return "recipe/recipeform";
+    }
+
+    @PostMapping("/")
+    public String saveOrUpdate(@ModelAttribute RecipeCommand command) {
+        RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
+
+        return "redirect:/recipe/show/" + savedCommand.getId();
     }
 }
