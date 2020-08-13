0a6a208ee38316c99b85c36e2598a3fd06284cc5
dzheleznyakov <zheleznyakov.dmitry@gmail.com>
2020-08-13 08:51:40 +0100

    Spring5 PetClinic: Adding builder pattern to PetType. #56

--- PetClinic/pet-clinic-data/src/main/java/zh/learn/spring5/petclinic/model/PetType.java
+++ PetClinic/pet-clinic-data/src/main/java/zh/learn/spring5/petclinic/model/PetType.java
@@ -13,13 +13,17 @@ import javax.persistence.Table;
 @Setter
 @Getter
 @NoArgsConstructor
-@AllArgsConstructor
-@Builder
 @Entity
 @Table(name = "types")
 public class PetType extends BaseEntity {
     @Column(name = "name")
     private String name;
+
+    @Builder
+    public PetType(Long id, String name) {
+        super(id);
+        this.name = name;
+    }
 
     @Override
     public String toString() {
