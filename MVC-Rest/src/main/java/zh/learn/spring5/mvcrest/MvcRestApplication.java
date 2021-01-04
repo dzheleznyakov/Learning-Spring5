d5118feb22616eac25dd27a265e302ca6bd9230f
dzheleznyakov <zheleznyakov.dmitry@gmail.com>
2021-01-04 12:24:23 +0000

    MVC-Rest first commit

---
+++ MVC-Rest/src/main/java/zh/learn/spring5/mvcrest/MvcRestApplication.java
@@ -0,0 +1,13 @@
+package zh.learn.spring5.mvcrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvcRestApplication.class, args);
+	}
+
+}
---
+++ MVC-Rest/src/test/java/zh/learn/spring5/mvcrest/MvcRestApplicationTests.java
@@ -0,0 +1,13 @@
+package zh.learn.spring5.mvcrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
