e8c5f0bf332b102abcdb9e6bd1c68f7f3af4f054
dzheleznyakov <zheleznyakov.dmitry@gmail.com>
2024-12-30 19:52:10 +0000

    Bootstrap

--- MVC-Rest/src/main/java/zh/learn/spring5/mvcrest/bootstrap/Bootstrap.java
+++ MVC-Rest/src/main/java/zh/learn/spring5/mvcrest/bootstrap/Bootstrap.java
@@ -36,6 +36,6 @@ public class Bootstrap implements CommandLineRunner {
         categoryRepository.save(exotic);
         categoryRepository.save(nuts);
 
-        System.out.println("Data Loaded =" + categoryRepository.count());
+        System.out.println("Data Loaded = " + categoryRepository.count());
     }
 }
